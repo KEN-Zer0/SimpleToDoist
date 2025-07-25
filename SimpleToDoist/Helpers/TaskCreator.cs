27a9e88b3877a24b82aa924f8591494b01425ac9
KEN_ZER0 <94794855+KEN-Zer0@users.noreply.github.com>
2025-07-25 21:36:45 +0200

    ogarniam get i set, nie jestem gupi

--- SimpleToDoist/Helpers/TaskCreator.cs
+++ SimpleToDoist/Helpers/TaskCreator.cs
@@ -3,22 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace SimpleToDoist.TasksCreation
 {
     public class TaskCreator
     {
-        public int taskIndex;
+        private int _taskIndex;
         public string taskName;
         public string taskTitle;
         public string taskDescription;
         public bool taskCompletion;
      
+        public int TaskIndex
+        {
+            get { return _taskIndex; }
+            set 
+            {
+                if (value < 0) 
+                { 
+                    MessageBox.Show($"Error index {_taskIndex} is negative");
+                }
+
+                _taskIndex = value;    
+            }
+        }
 
         public TaskCreator(int taskId, string newTaskName, 
             string newTaskTitle, string newTaskDescription)
         {
-            taskIndex = taskId;
+            TaskIndex = taskId;
             taskName = newTaskName + taskId.ToString();
             taskTitle = newTaskTitle;
             taskDescription = newTaskDescription;
